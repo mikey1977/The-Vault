0748d98c5e8eb569fdf9677ac7ed3ed0069aeb5c
mikey1977 <mmi@umich.edu>
2015-09-02 19:55:26 -1000

    return null if no key or invalid key is used, return value with appropriate key

--- vault.js
+++ vault.js
@@ -1,4 +1,21 @@
 'use strict';
 module.exports = function() {
-  
+  var myVault = {};
+
+  function _setValue(key, value) {
+    myVault[key] = value;
+
+  }
+  function _getValue(key) {
+    if (myVault.hasOwnProperty(key)) {
+      return myVault[key];
+    } else {
+      return null;
+    }
+  }
+
+  return {
+    setValue : _setValue,
+    getValue : _getValue
+  };
 };
